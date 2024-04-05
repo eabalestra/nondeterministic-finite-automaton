20b32010ce839731ae80e3f5693028e1adcb7a5c
matybq <matybq20@gmail.com>
2024-04-05 13:51:51 -0300

    working

--- main.c
+++ main.c
@@ -4,31 +4,44 @@
 int main()
 {
   NFA *nfa = create_nfa();
-  /*add_transition(nfa, 0, 1, 'a');
-  add_transition(nfa, 0, 2, 'a');
-  add_transition(nfa, 0, 0, 'b');
-  add_transition(nfa, 1, 2, 'b');
-  add_transition(nfa, 2, 2, 'b');
+
+  // AUTOMATA PANCHO
+  
+/*     add_transition(nfa, 0, 1, 'a');
+    add_transition(nfa, 0, 2, 'a');
+    add_transition(nfa, 0, 0, 'b');
+    add_transition(nfa, 1, 2, 'b');
+    add_transition(nfa, 2, 2, 'b');
+    add_transition(nfa, 1, 3, 'a');
+    add_transition(nfa, 3, 3, 'a');
+    add_transition(nfa, 3, 3, 'b');
+   */
+
+  // AUTOMATA MATI PARA CHECKEAR ROLLBACK
+  add_transition(nfa, 0, 1, 'a');
+  add_transition(nfa, 1, 1, 'a');
+  add_transition(nfa, 1, 1, 'b');
+  add_transition(nfa, 1, 4, 'a');
   add_transition(nfa, 1, 3, 'a');
+  add_transition(nfa, 0, 2, 'a');
+  add_transition(nfa, 2, 3, 'b');
   add_transition(nfa, 3, 3, 'a');
   add_transition(nfa, 3, 3, 'b');
+  set_accepting(nfa, 3, 1);
 
-  set_accepting(nfa, 3, 1);*/
-  // AUTOMATON EVEN 0's AND MULTIPLE 3 1's
+  // read_from_file(nfa, "/home/agustin/Desktop/university/automatas-y-lenguajes/repository/nondeterministic-finite-automaton/automatas/automata.dot");
 
-  read_from_file(nfa, "/home/agustin/Desktop/university/automatas-y-lenguajes/repository/nondeterministic-finite-automaton/automatas/automata.dot");
-  // read_from_file(nfa, "/home/matybq/UNRC/automatas/practica3/automata_ej4.dot");
-
-  //
-
-    printf("\n");
-    print_nfa(nfa);
-  char input[] = "abab";
+  printf("\n");
+  print_nfa(nfa);
+  char input[] = "aaaabbbabba";
   printf("\nInput string: %s\n", input);
   int accepted = belongs_nondet(nfa, input);
-  if (accepted) {
+  if (accepted)
+  {
     printf("The input string is accepted by the NFA\n");
-  } else {
+  }
+  else
+  {
     printf("The input string is not accepted by the NFA\n");
   }
 
